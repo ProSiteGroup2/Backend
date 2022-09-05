5480f00d47b705adf54ae0b0d76644614528f116
dhvihan <danulvihan@gmail.com>
2022-09-06 02:13:22 +0530

    simple p cartproduct

--- models/cart/cart.js
+++ models/cart/cart.js
@@ -1,38 +1,41 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-const CartProduct=require('./cartProduct');
-const Consumer=require('../consumer');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const CartProduct = require("./cartProduct");
+const Consumer = require("../consumer");
 
-const CartSchema=new Schema({
+const CartSchema = new Schema(
+	{
+		buyer_consumer: {
+			type: Schema.Types.ObjectId,
+			ref: "consumer",
+		},
 
-    buyer_consumer:{
-        type: Schema.Types.ObjectId,
-        ref: 'consumer'
-    },
+		buyer_labour: {
+			type: Schema.Types.ObjectId,
+			ref: "labour",
+		},
 
-    buyer_labour:{
-        type: Schema.Types.ObjectId,
-        ref: 'labour'
-    },
+		buyer_transporter: {
+			type: Schema.Types.ObjectId,
+			ref: "transporter",
+		},
 
-    buyer_transporter:{
-        type: Schema.Types.ObjectId,
-        ref: 'transporter'
-    },
+		buyer_contractor: {
+			type: Schema.Types.ObjectId,
+			ref: "contractor",
+		},
 
-    buyer_contractor:{
-        type: Schema.Types.ObjectId,
-        ref: 'contractor'
-    },
+		cartProducts: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "cartproduct",
+			},
+		],
 
-    cartProducts:[{
-        type: Schema.Types.ObjectId,
-        ref: 'cartProduct'
-    }],
+		totalPrice: Number,
+	},
+	{ timestamps: true }
+);
 
-    totalPrice:Number
-},{timestamps:true});
-
-const Cart=mongoose.model('cart',CartSchema);
-module.exports=Cart;
-
+const Cart = mongoose.model("cart", CartSchema);
+module.exports = Cart;
